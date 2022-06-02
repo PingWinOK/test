9d6183b4eedb89d84fa78382e6180d7e399b9fca
Boris Garbuzov <boris.garbuzov@gmail.com>
2022-06-03 01:40:12 +0300

    fix for mysql

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,10 @@
 # syntax=docker/dockerfile:1
 FROM python:3.7
 WORKDIR /code
+ENV FLASK_APP=app.py
+ENV FLASK_RUN_HOST=0.0.0.0
 COPY requirements.txt requirements.txt
-RUN pip install -r requirements.txt
-RUN python -m pip install PyMySQL
-RUN pip install PyMySQL
-RUN pip install pymysql
-RUN pip install --upgrade pip && \
-    pip install pymysql
-RUN docker-php-ext-install mysqli
-RUN python3 -m pip install PyMySQL
-RUN apk add --no-cache gcc musl-dev linux-headers
-
+RUN python -m pip install -r requirements.txt
 EXPOSE 5000
-COPY src/
-CMD ["python", "./app.py"]
+COPY . .
+CMD ["flask", "run"]
--- app.py
+++ app.py
@@ -3,8 +3,18 @@ import time
 import redis
 from flask import Flask
 
+import pymysql
+
 app = Flask(__name__)
 cache = redis.Redis(host='redis', port=6379)
+
+def connection():
+    s = 'db' #Your server(host) name 
+    d = 'MyDataBase' 
+    u = 'user' #Your login user
+    p = 'test' #Your login password
+    conn = pymysql.connect(host=s, user=u, password=p, database=d)
+    return conn
 
 def get_hit_count():
     retries = 5
@@ -19,5 +29,16 @@ def get_hit_count():
 
 @app.route('/')
 def hello():
-    count = get_hit_count()
-    return 'Hello World!I Danil and i slave this lab. I have been seen {} times.\n'.format(count)
+    # count = get_hit_count()
+    # return 'Hello World!I Danil and i slave this lab. I have been seen {} times.\n'.format(count)
+    users = []
+    conn = connection()
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM Person")
+
+    for row in cursor.fetchall():
+        # users.append({"id": row[0], "First_Name": row[1], "Last_Name": row[2]})
+        users.append(f"{row[0]} - {row[1]} - {row[2]} <br>")
+        print(row)
+    conn.close()
+    return '{}'.format("".join(users))
